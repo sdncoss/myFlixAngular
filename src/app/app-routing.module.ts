ee382215b759562e73e0aeeb344ac712fe0caf73
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-08-24 14:24:18 -0700

    changed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
