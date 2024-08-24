312a6b624ee1855ecc276fb6d63e69a586cb8e73
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-08-24 14:21:23 -0700

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 const routes: Routes = [
-  { path: 'profile', component: ProfilePageComponent },
+  { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
-  { path: '', redirectTo: 'movies', pathMatch: 'full' }
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
 @NgModule({
